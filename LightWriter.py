654407727d81d2449f4a085a1a659e09d011c2ec
Markus Rudel <markus.rudel@ruhr-uni-bochum.de>
2016-10-31 23:03:30 +0100

    moved variable creation

--- LightWriter.py
+++ LightWriter.py
@@ -23,7 +23,6 @@ class LightWriter:
     # Alternatively specify a hardware SPI connection on /dev/spidev0.0:
     SPI_PORT   = 0
     SPI_DEVICE = 0
-    pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
     # Configurable values
     filename = "hello.png"
@@ -49,6 +48,7 @@ class LightWriter:
 
     def main(self):
         try:
+            pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
             # Clear all the pixels to turn them off.
             pixels.clear()
             pixels.show()  # Make sure to call show() after changing any pixels!
