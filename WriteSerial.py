68fad5a6ca118d4027fde9dbaac831b63bf645c9
Markus Rudel <markus.rudel@ruhr-uni-bochum.de>
2016-11-01 18:55:58 +0100

    removed unnecessary configuration parameters

--- WriteSerial.py
+++ WriteSerial.py
@@ -5,8 +5,6 @@ ser = serial.Serial(
     port='/dev/ttyAMA0',
     baudrate=19200,
     parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_NONE,
-    bytesize=serial.EIGHTBITS,
     timeout=1
 )
 ser.open()
